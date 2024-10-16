c23b0499ab1e34aafd9b5397aa62d616d4be63e0
NandaYolanda06 <541231156@student.smktelkom-pwt.sch.id>
2024-10-16 14:52:58 +0700

    add new

---
+++ resources/views/index.blade.php
@@ -0,0 +1,43 @@
+<!DOCTYPE HTML>
+<html>
+<head>
+<title>Bio Data Pribadi</title>
+<link rel="stylesheet" href="asset/css/Profile.css">
+</head>
+<body style="background-image: url('asset/images backgrounds/background.jpg'); background-size: cover; background-position: center;"></body>
+
+<center>
+<img src="asset/images nda/Nanda.jpeg" width="150" height="150">
+<h1>Nanda Yolanda</h1>
+<p>
+<i><b>Mengenali Dirimu Sendiri</b></i>
+</p>
+<p>
+Profile |
+<a href="Gallery.html">Gallery</a> |
+<a href="Data.html">Data</a>
+</p>
+</center>
+<h2>Data Pribadi</h2>
+<h3>a. Data Saya</h3>
+<p>
+    Nama Lengkap : Nanda Yolanda || Tempat, Tanggal Lahir : Cilacap, 6 Agustus 2007 || No. Telp : 085702135810 || Instagram : @nda.nndaa_
+</p>
+<h3>b. Kelebihan</h3>
+<p>
+    Suka mendesain, Bisa membuat data, Bisa membayar pajak lewat online & Mudah bersosialisasi dengan orang baru
+    </p>
+    <h3>c. Kekurangan</h3>
+    <p>
+        Gampang badmood, Lemotan, Suka tidur & Lama bergaul
+    </p>
+    <h2>Pendidikan</h2>
+    Riwayat Pengalaman Latar Pendidikan</h2>
+    </body>
+    </html>
+    
+    <p>
+    Profile |
+    <a href="Gallery.html">Gallery</a> |
+    <a href="Data.html">Data</a>
+    </p>
--- routes/web.php
+++ routes/web.php
@@ -3,5 +3,21 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('index'); // merujuk ke file welcome.blade
+});
+
+Route::get('/', function () {
+    return view('Data'); // merujuk ke file welcome.blade
+});
+
+Route::get('/', function () {
+    return view('Gallery'); // merujuk ke file welcome.blade
+});
+
+Route::get('/welcome', function () {
+    return view('welcome'); // merujuk ke file welcome.blade
+});
+
+Route::get('/login', function () {
+    return view('login'); // merujuk ke file welcome.blade
 });
